b36948473427aee5a38c1545b53f67baf2247538
hsh0367 <hsh0367@gmail.com>
2024-12-03 11:45:44 +0900

    Refactor test.sh to remove leading and trailing whitespace in branch names

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 # 테스트할 문자열 설정
-input="  test/dev-merge
-    test/main-merge"
+input="test/dev-merge
+test/main-merge"
 
-IFS=$'\n' read -r -d '' -a branches <<< "$(echo "$input" | sed 's/^[[:space:]]*//; s/[[:space:]]*$//')"
+IFS=$'\n' read -r -d '' -a branches <<< "$input"
 for branch in "${branches[@]}"; do
   echo "Branch: '$branch'"
 done
